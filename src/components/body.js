a292dccfcde4b3e62f4bafeda1f85fab11e6e62a
Philip Hoang <philip.t.hoang@outlook.com>
2021-07-25 19:41:30 +0200

    Layout changes

--- src/components/body.js
+++ src/components/body.js
@@ -1,129 +1,158 @@
-import React, {Component, useCallback} from "react";
-import {Image, CloudinaryContext} from "cloudinary-react";
+import React, {Component} from "react";
+import {Image, CloudinaryContext } from "cloudinary-react";
 
-import lightGallery from "lightgallery";
+// import lightGallery from "lightgallery";
+
+//Grid 
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+
 
 // import styles
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
+// import lgThumbnail from 'lightgallery/plugins/thumbnail';
+
+import LazyLoad from 'react-lazy-load';
 
 
+import {mainImages} from './data.js';
+import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
 
-export default class Body extends Component {
-    
-    render() {
-        const onInit = () => {
-            console.log('lightGallery has been initialized');
-        }
+const useStyles = theme => ({
+	root: {
 
-        const items = [
-            {
-                id: "1",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945739/Analog/Thumbnails/R1-03298-004A_01_thumb_xwr5af.jpg"
-            },
-            {
-                id: "2",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945739/Analog/Thumbnails/R1-03298-015A_01_01_thumb_dbkvom.jpg"
-            },
-            {
-                id: "3",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-016A_01_thumb_f4idg1.jpg"
-            },
-            {
-                id: "4",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-020A_01_thumb_zdenzb.jpg" 
-            },
-            {
-                id: "5",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-012A_01_thumb_bz1vbc.jpg" 
-            },
-            {
-                id: "6",
-                src: "",
-                thumb: "https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-003A_01_thumb_wedvfg.jpg"
-            }
-        ];
+	  padding: '0 30px',
+	},
+	paper: {
+		height: 400,
+		width: 268,
+		outline: "1px dashed orange"
+  
+	  },
+	  gutter: {
+		marginLeft: 20,
+		"&:first-child": {
+		  marginLeft: 0
+		},
+		[theme.breakpoints.down('md')]: {
+			marginBottom: 20,
+		},
+	  }
+	
+  });
 
-        function getItems() {
-            return items.map((item) => {
-                return (
-                    <a
-                        key={item.id}
-                        data-lg-size={item.size}
-                        className="gallery-item"
-                        data-src={item.src}
-                    >
-                        <img className="img-responsive" src={item.thumb} />
-                    </a>
-                );
-            });
-        }
-        return (
+  
+class Body extends Component {
+      render() {
+        const { classes } = this.props;
+          
+          return (
+            
             <React.Fragment>
                 <CloudinaryContext cloudName="philipscloud">
-                <div className="container">
-                    <div className="row mt-3"> {/*Start row 1 */}
-                        <div className="col mt-3">
-                        <Image 
-                            publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945739/Analog/Thumbnails/R1-03298-004A_01_thumb_xwr5af.jpg"
-                            width="350" //350 is max for three images for each row in Bootstrap(with 15px paddding)
-                            loading="lazy"
-                        />
-                        </div> 
-                        <div className="col mt-3">
-                            <Image 
-                                publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945739/Analog/Thumbnails/R1-03298-015A_01_01_thumb_dbkvom.jpg" 
-                                width="350"
-                                loading="lazy"
-                                /> 
-                        </div>
-                        <div className="col mt-3">
-                            <Image 
-                                publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-016A_01_thumb_f4idg1.jpg" 
-                                width="350"
-                                loading="lazy"
-                            /> 
-                        </div>
-                    </div> {/*End row 1*/}
-                    <div className="row mt-3"> {/*Start row 2*/}
-                        <div className="col mt-3">
-                            <Image 
-                                publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-020A_01_thumb_zdenzb.jpg" 
-                                width="350"
-                                loading="lazy"
-                            />
-                        </div>
-                        <div className="col mt-3">
-                            <Image 
-                                publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-012A_01_thumb_bz1vbc.jpg" 
-                                width="350"
-                                loading="lazy"
-                            />
-                        </div>
-                        <div className="col mt-3">
-                            <Image 
-                                publicId="https://res.cloudinary.com/philipscloud/image/upload/v1625945738/Analog/Thumbnails/R1-03298-003A_01_thumb_wedvfg.jpg" 
-                                width="350"
-                                loading="lazy"
-                            />
-                        </div>
-                    </div> {/*End row 2*/}
+                    <Grid container direction={"row"} justifyContent="center" className={classes.root} spacing={2}>
+                        <Grid item container justifyContent="center">
 
-                    {/* <lightGallery elementClassNames="custom-class-name" onInit={onInit}>
-                        {getItems()}
-                    </lightGallery> */}
-                </div>
+							<Grid className={classes.gutter}>
+                          		<Image publicId={mainImages[0].url} width={400} alt={mainImages[0].alt}/>
+							</Grid>
 
+							<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[1].url} width={400} alt={mainImages[1].alt}/>
+							</Grid>
+
+					  		<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[2].url} width={400} alt={mainImages[2].alt}/>
+							</Grid> 
+
+                        </Grid>  
+
+                        <Grid item container justifyContent="center">
+
+							<Grid className={classes.gutter}>
+                          		<Image publicId={mainImages[3].url} width={400} alt={mainImages[3].alt}/>
+							</Grid>
+
+							<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[4].url} width={400} alt={mainImages[4].alt}/>
+							</Grid>
+
+					  		<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[5].url} width={400} alt={mainImages[5].alt}/>
+							</Grid> 
+
+                        </Grid>  
+						<Grid item container justifyContent="center">
+
+							<Grid className={classes.gutter}>
+                          		<Image publicId={mainImages[6].url} width={400} alt={mainImages[6].alt}/>
+							</Grid>
+
+							<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[7].url} width={400} alt={mainImages[7].alt}/>
+							</Grid>
+
+					  		<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[8].url} width={400} alt={mainImages[8].alt}/>
+							</Grid> 
+
+                        </Grid>  
+						<Grid item container justifyContent="center">
+
+							<Grid className={classes.gutter}>
+                          		<Image publicId={mainImages[9].url} width={400} alt={mainImages[9].alt}/>
+							</Grid>
+
+							<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[10].url} width={400} alt={mainImages[10].alt}/>
+							</Grid>
+
+					  		<Grid className={classes.gutter}>
+							  	<Image publicId={mainImages[11].url} width={400} alt={mainImages[11].alt}/>
+							</Grid> 
+                        </Grid>  
+
+						<Grid item container justifyContent="center">
+
+							<Grid className={classes.gutter}>
+                          		<Image publicId={mainImages[12].url} width={400} alt={mainImages[12].alt}/>
+							</Grid>
+
+						
+
+					
+                        </Grid>  
+
+                    </Grid>
                 </CloudinaryContext>
             </React.Fragment>
         )
     }
 }
+
+// Loop through images 
+//
+// {mainImages && mainImages.map((image, index) => {
+//   return(
+//     <div key={index}>
+//       <LazyLoad 
+//         width={400}>
+//           <Grid container item xs={12} spacing={3}>
+//             <Image publicId={image.url} width="400" height="auto"></Image>
+//           </Grid>
+//       </LazyLoad>
+//     </div>
+//   )
+// })}
+
+Body.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(useStyles)(Body);
+
