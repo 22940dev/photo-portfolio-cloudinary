e16b5f4bfa4b908f05eed111e2e9580418c8c769
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-08-21 08:32:35 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import logo from "../assets/logo512.png";
 
 export default class Header extends Component {
     render() {
         return <React.Fragment>
-            <header id="header">
-            <div container>
-                <div className="d-flex flex-row justify-content-center pt-4 pb-4">
-                    <img src={logo} width="50px" height="50px"></img>
-                    <h1>PHILIP HOANG</h1>
-                </div>
-            </div>
-            </header>
-        </React.Fragment>
+                 <header id="header">
+                    <div container>
+                    <div className="d-flex flex-row justify-content-center pt-4 pb-4">
+                        <img src={logo} width="50px" height="50px"></img>
+                            <h1>PHILIP HOANG</h1>
+                    </div>
+                    </div>
+                </header>
+            </React.Fragment>
     }
 }
