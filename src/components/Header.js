794fa6cd5d54af47d3cea4efc3ba0dfdddac8bab
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-09-01 23:23:11 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default class Header extends Component {
                     <div container>
                     <div className="d-flex flex-row justify-content-center pt-4 pb-4">
                         <img src={logo} width="50px" height="50px" alt="" />
-                            <h1>PHILIP HOANG</h1>
+                            <h1>MY PHOTOBLOG</h1>
                     </div>
                     </div>
                 </header>
